0f2b8c63a97df73eb9b06262a9bd2fe654a4f1e6
sanjeenchhaperwal <sanjeen0chhaperwal@gmail.com>
2024-11-19 18:07:26 +0530

    completed layout 40%

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#04152D',
+        'light-blue': '#262a34',
+        'blue-gray': '#363a45',
+         'dark-black': '#0C0C0C99'
+    },
+    fontFamily :{
+          "outfit" : ['Outfit', 'sans-serif'],
+          "roboto" : ['Roboto','sans-serif']
+          // font-family: "Roboto", sans-serif;
+    }
+  },
+  plugins: [],
+}
+}
+
